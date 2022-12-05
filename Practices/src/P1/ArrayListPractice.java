081cb53dc1f5552e52b652d756f5e3a5e75fd492
aymansalem22 <s.aymansalem@gmail.com>
2022-12-05 13:19:25 +0100

    update more

--- Practices/src/P1/ArrayListPractice.java
+++ Practices/src/P1/ArrayListPractice.java
@@ -1,19 +1,29 @@
 package P1;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class ArrayPractice {
+public class ArrayListPractice {
 
 	public static void main(String[] args) {
-		List<Integer> list=new ArrayList<>();
+		List<Integer> list = new ArrayList<>();
 		list.add(1);
 		list.add(2);
 		list.add(3);
+		// Update an element using the set(int index, E e) method
+		list.set(1, 4);
+		// Check if an element is present using the contains(Object o) method
+		System.out.println(list.contains(7));
+		// Remove all elements within a given Collection using the
+		// removeAll(Collection<?> c) method
+		// list.removeAll(list);
 		System.out.println(list);
-		System.out.println("The element at index two is "+list.get(1));
-		System.out.println("This size of the list is "+list.size());
-
+		System.out.println("The element at index two is " + list.get(1));
+		System.out.println("This size of the list is " + list.size());
+		// Replace all elements using the replaceAll(UnaryOperator<E> operator)
+		Collections.replaceAll(list, 3, 22);
+		System.out.println(list);
 	}
 
 }
--- Practices/src/P1/LinkedListPrac.java
+++ Practices/src/P1/LinkedListPrac.java
@@ -1,5 +1,55 @@
 package P1;
 
-public class More {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 
+public class LinkedListPrac  {
+	
+public static void main(String[] args) {
+ LinkedList<Integer> linkedList=new LinkedList<>();
+ linkedList.add(1);
+ linkedList.add(2);
+ linkedList.addLast(3);
+ System.out.println(linkedList);
+ linkedList.addFirst(10);
+ System.out.println(linkedList);
+ linkedList.add(2,20);
+ System.out.println(linkedList);
+ List<Integer> list=new ArrayList<>();
+ list.add(101);
+ list.add(102);
+ list.add(103);
+ linkedList.addAll(3,list);
+ System.out.println(linkedList);
+ //Fetch an element at a particular index using the get(int index) method
+ System.out.println(list.get(2));
+ //Remove the last element using the removeLast() method
+ linkedList.removeLast();
+ System.out.println(linkedList);
+ //Sort a LinkedList using the sort() method
+ linkedList.sort(Comparator.reverseOrder());
+ System.out.println(linkedList);
+ 
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
