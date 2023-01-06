82c609e5aaaf970959c8190624da6de76134f178
aymansalem22 <s.aymansalem@gmail.com>
2023-01-06 03:10:11 +0100

    update..

--- Practices/src/P3/arrays1.java
+++ Practices/src/P3/arrays1.java
@@ -2,26 +2,14 @@ package P3;
 
 //concept arrays 
 public class arrays1 {
-public static void main(String[] args) {
-	int [] myArray=new int[10];// Declaration and instantiation of array with legth 10
-	for(int i=0;i<myArray.length;i++) 
-	{
-		myArray[i]=i+1;//Initialize values to 1-10
-	}
-	for(int i=0;i<myArray.length;i++) {
-		System.out.println("The value at myArray["+i+"] is: "+myArray[i]);
-		//printing all values to console
+	public static void main(String[] args) {
+		int[] myArray = new int[10];// Declaration and instantiation of array with legth 10
+		for (int i = 0; i < myArray.length; i++) {
+			myArray[i] = i + 1;// Initialize values to 1-10
+		}
+		for (int i = 0; i < myArray.length; i++) {
+			System.out.println("The value at myArray[" + i + "] is: " + myArray[i]);
+			// printing all values to console
+		}
 	}
 }
-}
-
-
-
-
-
-
-
-
-
-
-
