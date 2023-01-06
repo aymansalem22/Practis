bd9da69a9dae13a8f023408da7e5566174003dfc
aymansalem22 <s.aymansalem@gmail.com>
2023-01-06 13:00:08 +0100

    arrays string

---
+++ Practices/src/P3/strArray.java
@@ -0,0 +1,15 @@
+package P3;
+
+public class strArray {
+	public static void main(String[] args) {
+
+		String[] strArray = { "A", "Quick", "Brown", "Fox", "Jumps", "Over", "a", "Lazy", "Dog"
+
+		};// initialization of first string array
+
+		System.out.println("The Values stored in strArray are: ");
+		for (int i = 0; i < strArray.length; i++) {
+			System.out.println(strArray[i] + " ");
+		}
+	}
+}
