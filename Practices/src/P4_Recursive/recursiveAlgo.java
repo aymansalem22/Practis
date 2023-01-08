49b96151172a0a6e9c51947b0d2d5e527be9cc90
aymansalem22 <s.aymansalem@gmail.com>
2023-01-09 00:31:33 +0100

    Generic recursive algorithm

---
+++ Practices/src/P4_Recursive/recursiveAlgo.java
@@ -0,0 +1,14 @@
+package P4_Recursive;
+
+public class recursiveAlgo {
+
+	RecursiveMethod(){
+	//Base Case
+if(base case condition) {
+return some base case value;
+}
+else {
+	// Recursive Case
+	return (some work and then a recursive call to RecursiveMethod())
+}}
+}
