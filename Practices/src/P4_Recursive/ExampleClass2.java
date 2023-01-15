fc2291e5d262f7f2495baa006d8794d288ef2aec
aymansalem22 <s.aymansalem@gmail.com>
2023-01-15 23:32:20 +0100

    Recursive Problem concept

---
+++ Practices/src/P4_Recursive/ExampleClass2.java
@@ -0,0 +1,19 @@
+package P4_Recursive;
+//Recursive Problem concept
+public class ExampleClass2 {
+	private static void printNum(int n) {
+		// base case
+		if (n == 0) {
+			return;
+		}
+
+		// Recursive case
+		printNum(n - 1);
+		System.out.print(n + " ");
+
+	}
+
+	public static void main(String[] args) {
+		printNum(6);
+	}
+}
