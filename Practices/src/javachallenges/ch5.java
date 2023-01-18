9f8251b3ff623621c80c9d3af975a19576ee33d3
aymansalem22 <s.aymansalem@gmail.com>
2023-01-18 11:45:51 +0100

    MegaBytes Converter

---
+++ Practices/src/javachallenges/ch5.java
@@ -0,0 +1,23 @@
+package javachallenges;
+
+//Coding Exercise 3: MegaBytes Converter
+public class ch5 {
+
+	public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+		int megabyte = Math.round(kiloBytes) / 1024;
+		int reminingkb = (kiloBytes - (1024 * Math.round(megabyte)));
+
+		if (kiloBytes < 0)
+			System.out.println("Invalid Value");
+
+		else
+
+			System.out.println(kiloBytes + " KB = " + megabyte + " MB and " + reminingkb + " KB");
+	}
+
+	public static void main(String[] args) {
+		printMegaBytesAndKiloBytes(5000);
+		printMegaBytesAndKiloBytes(2500);
+		printMegaBytesAndKiloBytes(-2500);
+	}
+}
