681b38f1658e39ff1fff7d3c531d57f1551c5868
aymansalem22 <s.aymansalem@gmail.com>
2023-03-28 02:18:16 +0200

    LocalDateTime

---
+++ Practices/src/feature_java8_javatimeapi/LocalDateTimeDemo.java
@@ -0,0 +1,37 @@
+package feature_java8_javatimeapi;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class LocalDateTimeDemo {
+
+	public static void main(String[] args) {
+		LocalDateTime date = LocalDateTime.now();
+		System.out.println(date);
+
+		System.out.println("getting specfic time -->");
+		LocalDateTime specDate = LocalDateTime.of(2019, 05, 03, 12, 34);
+		System.out.println(specDate);
+
+		specDate = LocalDateTime.of(2019, Month.AUGUST, 03, 23, 34);
+		System.out.println(specDate);
+
+		System.out.println("Using parse --> ");
+		LocalDateTime paDate = LocalDateTime.parse("2020-06-20T07:54:00");
+		System.out.println(paDate);
+
+		System.out.println("Given date plus -->");
+		LocalDateTime givenDate = LocalDateTime.parse("2020-05-12T08:30:00").plusDays(4);
+		System.out.println(givenDate);
+
+		givenDate = LocalDateTime.parse("2020-05-12T08:30:00").plus(4, ChronoUnit.MONTHS);
+		System.out.println(givenDate);
+
+		givenDate = LocalDateTime.parse("2020-05-12T08:30:00").minusMonths(4);
+		System.out.println(givenDate);
+
+	}
+
+}
